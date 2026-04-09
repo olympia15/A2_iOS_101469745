dc4ee8cd7356113390f48be20990780fbc934943
olympia15 <olympiamichelle15@gmail.com>
2026-04-09 14:24:07 -0400

    Environment object addd, FetchRequested created, and filteredProducts function added in SearchView

--- A2_iOS_101469745/SearchView.swift
+++ A2_iOS_101469745/SearchView.swift
@@ -5,3 +5,34 @@
 //  Created by Olympia Santaguida-Antunes on 2026-04-09.
 //
 
+import SwiftUI
+import CoreData
+
+struct SearchView: View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Product.name, ascending: true)],
+        animation: .default
+    ) private var products: FetchedResults<Product>
+
+    @State private var searchText: String = ""
+
+    var filteredProducts: [Product] {
+        if searchText.isEmpty {
+            return Array(products)
+        } else {
+            return products.filter {
+                ($0.name ?? "").localizedCaseInsensitiveContains(searchText) ||
+                ($0.productDescription ?? "").localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+    
+    var body: some View {
+        
+    }
+    
+    
+}
