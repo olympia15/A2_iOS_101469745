4974133d70ff2dee2837c89ba8ae029e94cfbc4f
olympia15 <olympiamichelle15@gmail.com>
2026-04-09 12:58:25 -0400

    added container, loadPersistentStores, and helper function to Persistence.swift

--- A2_iOS_101469745/Persistence.swift
+++ A2_iOS_101469745/Persistence.swift
@@ -18,5 +18,23 @@ struct PersistenceController {
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
+        container.loadPersistentStores{ _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        
+        seedProductsIfNeeded()
+    }
+    
+    // helper function to seed db
+    private func seedProductsIfNeeded() {
+        let context = container.viewContext
+        
+        // only seed if the database is empty
+        let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
+        let count = (try? context.count(for: fetchRequest)) ?? 0
+        guard count == 0 else { return }
     }
 }
