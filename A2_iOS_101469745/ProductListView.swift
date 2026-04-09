e5376c93345fe500ef120e313dd78dc944bb08bf
olympia15 <olympiamichelle15@gmail.com>
2026-04-09 14:05:11 -0400

    view body added to ProductListView

--- A2_iOS_101469745/ProductListView.swift
+++ A2_iOS_101469745/ProductListView.swift
@@ -5,11 +5,32 @@
 //  Created by Olympia Santaguida-Antunes on 2026-04-09.
 //
 
+import SwiftUI
+import CoreData
+
 struct ProductListView: View {
     
     @FetchRequest(
             sortDescriptors: [NSSortDescriptor(keyPath: \Product.name, ascending: true)],
             animation: .default
         ) private var products: FetchedResults<Product>
+    
+    var body: some View {
+        NavigationView{
+            List(products) {product in
+                VStack(alignment: .leading, spacing: 6){
+                    Text(product.name ?? "Unknown")
+                        .font(.headline)
+                    
+                    Text(product.productDescription ?? "No description available.")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        .lineLimit(2)
+                }
+                .padding(.vertical, 4)
+            }
+            .navigationTitle("All Products")
+        }
+    }
     
 }
