e42359eb56e57ccd3c18f09f391946ebf656f9b4
olympia15 <olympiamichelle15@gmail.com>
2026-04-09 12:07:18 -0400

    Initial Commit

---
+++ A2_iOS_101469745/A2_iOS_101469745App.swift
@@ -0,0 +1,21 @@
+//
+//  A2_iOS_101469745App.swift
+//  A2_iOS_101469745
+//
+//  Created by Olympia Santaguida-Antunes on 2026-04-09.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct A2_iOS_101469745App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
